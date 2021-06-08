f4acd71c9e3c3a6c494fadef6b625eac793d0bfc
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-06-08 17:00:18 -0400

    Created function templates

---
+++ app.js
@@ -0,0 +1,64 @@
+//Henry Baum
+//CSCI-39548-01
+//Prof. Melissa Lynch
+//Assignment #2: JavaScript
+
+//My custom implementation of Array.prototype.forEach() 
+Array.prototype.myEach = () => {
+
+}
+
+//My custom implementation of Array.prototype.map() 
+Array.prototype.myMap = () => {
+
+}
+
+//My custom implementation of Array.prototype.filter() 
+Array.prototype.myFilter = () => {
+
+}
+
+//My custom implementation of Array.prototype.some() 
+Array.prototype.mySome = () => {
+
+}
+
+//My custom implementation of Array.prototype.every() 
+Array.prototype.myEvery = () => {
+
+}
+
+//My custom implementation of Array.prototype.reduce() 
+Array.prototype.myReduce = () => {
+
+}
+
+//My custom implementation of Array.prototype.includes() 
+Array.prototype.myIncludes = () => {
+
+}
+
+//My custom implementation of Array.prototype.indexOf() 
+Array.prototype.myIndexOf = () => {
+
+}
+
+//My custom implementation of Array.prototype.push() 
+Array.prototype.myPush = () => {
+
+}
+
+//My custom implementation of Array.prototype.lastIndexOf() 
+Array.prototype.myLastIndexOf = () => {
+
+}
+
+//My custom implementation of Object.keys() 
+Object.grabKeys = () => {
+
+}
+
+//My custom implementation of Object.values() 
+Object.grabValues = () => {
+
+}
